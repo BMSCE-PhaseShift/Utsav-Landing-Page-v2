b28618aba4d947e5629691ea7712438f5789c6a1
Rahul Anil Nayak <whorahulnayak@gmail.com>
2023-05-21 14:09:34 +0530

    working ccommit

--- sections/Teams.jsx
+++ sections/Teams.jsx
@@ -92,7 +92,7 @@ const Teams = () => {
     ];
 
     return (
-        <section className={`${styles.paddings} relative z-10`} id="about">
+        <section className={`${styles.paddings} relative z-10`} id="team">
             <motion.div
                 variants={staggerContainer}
                 initial="hidden"
@@ -105,30 +105,7 @@ const Teams = () => {
                     textStyles="text-center cinzel-font text-hp-yellow"
                 />
 
-                <TypingText title="They did something" textStyles="text-center" />
-
-                {/* <div className="grid gap-4 lg:grid-cols-8 md:grid-cols-6 sm:grid-cols-4">
-                    {images.map((image, index) => (
-                        // <motion.img
-                        //     key={index}
-                        //     src={image}
-                        //     alt={`Image ${index + 1}`}
-                        //     className="col-span-1 w-[150px] h-[150px] object-cover"
-                        // />
-                        <div
-                            key={index}
-                            className="aspect-w-1 aspect-h-1"
-                        >
-                            <div className="w-[150px] h-[150px]">
-                                <img
-                                    src={image}
-                                    alt={`Image ${index + 1}`}
-                                    className="object-cover w-full h-full"
-                                />
-                            </div>
-                        </div>
-                    ))}
-                </div> */}
+                <TypingText title="The ones responsible for an amazing Utsav-23" textStyles="text-center" />
 
                 <div className={` ${styles.topPaddings} grid grid-cols-8 gap-0 lg:grid-cols-12 md:grid-cols-8 sm:grid-cols-8`}>
                     {images.map((image, index) => (
@@ -136,19 +113,9 @@ const Teams = () => {
                             key={index}
                             src={image}
                             alt={`Image ${index + 1}`}
-                            className="object-cover w-[50px] h-[50px] lg:w-[100px] lg:h-[100px] md:w-[90px] md:h-[80px] sm:w-[50px] sm:h-[50px] border"
+                            className="brightness-100 hover:brightness-125 object-cover w-[97px] h-[80px] lg:w-[97px] lg:h-[80px] md:w-[90px] md:h-[80px] sm:w-[80px] sm:h-[80px] "
                         />
                     ))}
-
-                    {/* THIS IS FOR LOOPING OVER SAME IMAGE N TIMES WITHOUT AN ARRAY */}
-                    {/* {Array(80).fill().map((_, index) => (
-                        <motion.img
-                            key={index}
-                            src="/founder.jpg"
-                            alt={`Image ${index + 1}`}
-                            className="object-cover w-[50px] h-[50px] lg:w-[100px] lg:h-[100px] md:w-[90px] md:h-[80px] sm:w-[50px] sm:h-[50px] border"
-                        />
-                    ))} */}
                 </div>
             </motion.div>
         </section>
