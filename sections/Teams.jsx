38ff2c90b40479400947923c84c2b7239a4cc781
Rahul Anil Nayak <whorahulnayak@gmail.com>
2023-05-21 14:48:44 +0530

    base64 image

--- sections/Teams.jsx
+++ sections/Teams.jsx
@@ -1,96 +1,12 @@
 "use client";
 
 import { motion } from "framer-motion";
-
 import { TitleText, TypingText } from "../components";
 import styles from "../styles";
 import { fadeIn, staggerContainer } from "../utils/motion";
+import data from "../output.json";
 
 const Teams = () => {
-    // Array of image URLs
-    const images = [
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-        '/founder.jpg',
-    ];
-
     return (
         <section className={`${styles.paddings} relative z-10`} id="team">
             <motion.div
@@ -108,11 +24,11 @@ const Teams = () => {
                 <TypingText title="The ones responsible for an amazing Utsav-23" textStyles="text-center" />
 
                 <div className={` ${styles.topPaddings} grid grid-cols-8 gap-0 lg:grid-cols-12 md:grid-cols-8 sm:grid-cols-8`}>
-                    {images.map((image, index) => (
+                    {data.map((image, index) => (
                         <motion.img
                             key={index}
-                            src={image}
-                            alt={`Image ${index + 1}`}
+                            src={`data:image/png;base64,${image.encoding}`}
+                            alt={`${image.name}`}
                             className="brightness-100 hover:brightness-125 object-cover w-[97px] h-[80px] lg:w-[97px] lg:h-[80px] md:w-[90px] md:h-[80px] sm:w-[80px] sm:h-[80px] "
                         />
                     ))}
