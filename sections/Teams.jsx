46c18684c282361efdaca3f451571910528fca44
Rahul Anil Nayak <whorahulnayak@gmail.com>
2023-05-21 15:14:15 +0530

    image tile improved

--- sections/Teams.jsx
+++ sections/Teams.jsx
@@ -5,8 +5,18 @@ import { TitleText, TypingText } from "../components";
 import styles from "../styles";
 import { fadeIn, staggerContainer } from "../utils/motion";
 import data from "../output.json";
+import { useState } from "react";
+
+const shuffleArray = (array) => {
+    const newArray = [...array];
+    newArray.sort(() => Math.random() - 0.5);
+    return newArray;
+  };
 
 const Teams = () => {
+    // write code to randomise the order of images
+    const [imageData, setImageData] = useState(shuffleArray(data));
+
     return (
         <section className={`${styles.paddings} relative z-10`} id="team">
             <motion.div
@@ -23,13 +33,13 @@ const Teams = () => {
 
                 <TypingText title="The ones responsible for an amazing Utsav-23" textStyles="text-center" />
 
-                <div className={` ${styles.topPaddings} grid grid-cols-8 gap-0 lg:grid-cols-12 md:grid-cols-8 sm:grid-cols-8`}>
-                    {data.map((image, index) => (
+                <div className={` ${styles.topPaddings} grid grid-cols-6 gap-0 sm:grid-cols-6 md:grid-cols-6 lg:grid-cols-12 `}>
+                    {imageData.map((image, index) => (
                         <motion.img
                             key={index}
                             src={`data:image/png;base64,${image.encoding}`}
                             alt={`${image.name}`}
-                            className="brightness-100 hover:brightness-125 object-cover w-[97px] h-[80px] lg:w-[97px] lg:h-[80px] md:w-[90px] md:h-[80px] sm:w-[80px] sm:h-[80px] "
+                            className="brightness-100 hover:brightness-125 object-cover w-[97px] h-[80px] lg:w-[97px] lg:h-[80px] md:w-[97px] md:h-[80px] sm:w-[97px] sm:h-[80px] "
                         />
                     ))}
                 </div>
