ddb16d561ed7242ff4dd6d7dd8f2d5783a42f069
Rahul Anil Nayak <whorahulnayak@gmail.com>
2023-05-22 22:06:48 +0530

    ok

--- pages/index.js
+++ pages/index.js
@@ -34,8 +34,8 @@ const Page = ({data}) => (
 export default Page;
 
 export async function getServerSideProps(context) {
-    // const response = await fetch('https://bmsutsav.in/api/files');
-    const response = await fetch('http://localhost:3000/api/files');
+    const response = await fetch('https://bmsutsav.in/api/files');
+    // const response = await fetch('http://localhost:3000/api/files');
 
     const data = await response.json();
     return{
