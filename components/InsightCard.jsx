4042679cf898fdfcd26c9d9f098b94ffc6c3ed96
Rahul Anil Nayak <whorahulnayak@gmail.com>
2023-05-23 18:47:58 +0530

    im so done with website work

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -13,6 +13,14 @@ const InsightCard = ({ name, role, index }) => (
 	>
 		<div className="w-full flex flex-1 justify-start items-center sm:flex-col flex-row">
 			<div className="flex-1 mx-[15px] my-[10px] md:ml-[50px] flex flex-col justify-center">
+				{/* add Image */}
+				{/* <Image
+					src={"/output/001-rahul.jpg"}
+					alt="arrow"
+					width={120}
+					height={120}
+					className="w-[120px] h-[120px] object-cover"
+				/> */}
 				<p className="font-normal lg:text-[20px] text-[18px] text-white text-center">
 					{name}
 				</p>
