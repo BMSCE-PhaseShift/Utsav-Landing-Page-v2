d2894c87638fdd4edba5b372fc2c72200db38f8e
Navaneeth Suresh <97603264+itsNavaneeth@users.noreply.github.com>
2023-05-28 13:42:02 +0530

    styling partner changes

--- sections/Sponsors.jsx
+++ sections/Sponsors.jsx
@@ -50,8 +50,8 @@ const Sponsors = () => {
 					{Object.keys(cat2).map((item, index) => (
 						<div
 							key={"spon" + index}
-							//   className="basis-1/2 col-4 col-md-auto text-center flex md:flex-col justify-center"
-							//   className="basis-1/2"
+						//   className="basis-1/2 col-4 col-md-auto text-center flex md:flex-col justify-center"
+						//   className="basis-1/2"
 						>
 							<br />
 							<a href={cat2[item]} target="_blank" rel="noreferrer">
@@ -76,7 +76,7 @@ const Sponsors = () => {
 					{Object.keys(cat3).map((item, index) => (
 						<div
 							key={"spon" + index}
-							//   className="col-4 col-md-6 text-center flex md:flex-col justify-center"
+						//   className="col-4 col-md-6 text-center flex md:flex-col justify-center"
 						>
 							<br />
 							<a href={cat3[item]} target="_blank" rel="noreferrer">
@@ -100,7 +100,7 @@ const Sponsors = () => {
 					{cat4.map((item, index) => (
 						<div
 							key={"spon" + index}
-							//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
+						//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
 						>
 							<br />
 							<a href={cat4[item]} target="_blank" rel="noreferrer">
@@ -114,116 +114,136 @@ const Sponsors = () => {
 				</div>
 				<br />
 				<br />
-					<SponsorText
-						title={<>Hospitality Sponsors</>}
-						textStyles="text-center cinzel-font text-amber-200"
-					/>
-					<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 justify-items-center">
-						<div
-							key={"spon"}
-							//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
-						>
-							<br />
-							<a href={""} target="_blank" rel="noreferrer">
-								<img
-									src={"/logos/" +"ITC-Limited_Hospitality.jpg"}
-									className="max-w-32 max-h-32"
-								></img>
-							</a>
-						</div>
-						<div
-							key={"spon"}
-							//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
-						>
-							<br />
-							<a href={""} target="_blank" rel="noreferrer">
-								<img
-									src={"/logos/" +"Daadis_Hospitality.png"}
-									className="max-w-32 max-h-32"
-								></img>
-							</a>
-						</div>
+				<SponsorText
+					title={<>Hospitality Sponsors</>}
+					textStyles="text-center cinzel-font text-amber-200"
+				/>
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 justify-items-center">
+					<div
+						key={"spon"}
+					//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
+					>
+						<br />
+						<a href={""} target="_blank" rel="noreferrer">
+							<img
+								src={"/logos/" + "ITC-Limited_Hospitality.jpg"}
+								className="max-w-32 max-h-32"
+							></img>
+						</a>
 					</div>
-					<br />
-					<br />
-					<SponsorText
-						title={<>Energy Drink Partner</>}
-						textStyles="text-center cinzel-font text-amber-200"
-					/>
-					<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
-						<div
-							key={"spon"}
-							//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
-						>
-							<br />
-							<a href={""} target="_blank" rel="noreferrer">
-								<img
-									src={"/logos/" +"red-bull_Amber.png"}
-									className="max-w-32 max-h-32"
-								></img>
-							</a>
-						</div>
-						</div>
-					<br />
-					<br />
-					<SponsorText
-						title={<>Sustainibility Sponsor</>}
-						textStyles="text-center cinzel-font text-amber-200"
-					/>
-					<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
-						<div
-							key={"spon"}
-							//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
-						>
-							<br />
-							<a href={""} target="_blank" rel="noreferrer">
-								<img
-									src={"/logos/" +"Eat-Raja_sustainability.jpg"}
-									className="max-w-32 max-h-32"
-								></img>
-							</a>
-						</div>
+					<div
+						key={"spon"}
+					//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
+					>
+						<br />
+						<a href={""} target="_blank" rel="noreferrer">
+							<img
+								src={"/logos/" + "Daadis_Hospitality.png"}
+								className="max-w-32 max-h-32"
+							></img>
+						</a>
 					</div>
-					<br />
-					<br />
-					<SponsorText
-						title={<>Radio Partner</>}
-						textStyles="text-center cinzel-font text-amber-200"
-					/>
-					<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
-						<div
-							key={"spon"}
-							//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
-						>
-							<br />
-							<a href={""} target="_blank" rel="noreferrer">
-								<img
-									src={"/logos/" +"indigo-music_Hospitality.jpg"}
-									className="max-w-32 max-h-32"
-								></img>
-							</a>
-						</div>
+				</div>
+				<br />
+				<br />
+				<SponsorText
+					title={<>Energy Drink Partner</>}
+					textStyles="text-center cinzel-font text-amber-200"
+				/>
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
+					<div
+						key={"spon"}
+					//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
+					>
+						<br />
+						<a href={""} target="_blank" rel="noreferrer">
+							<img
+								src={"/logos/" + "red-bull_Amber.png"}
+								className="max-w-32 max-h-32"
+							></img>
+						</a>
 					</div>
-					<br />
-					<br />
-					<SponsorText
-						title={<>Social Media Partner</>}
-						textStyles="text-center cinzel-font text-amber-200"
-					/>
-					<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
-						<div
-							key={"spon"}
-							//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
-						>
-							<br />
-							<a href={""} target="_blank" rel="noreferrer">
-								<img
-									src={"/logos/" +"Namma-Karnataka_social.jpg"}
-									className="max-w-32 max-h-32"
-								></img>
-							</a>
-						</div>
+				</div>
+				<br />
+				<br />
+				<SponsorText
+					title={<>Sustainibility Sponsor</>}
+					textStyles="text-center cinzel-font text-amber-200"
+				/>
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
+					<div
+						key={"spon"}
+					//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
+					>
+						<br />
+						<a href={""} target="_blank" rel="noreferrer">
+							<img
+								src={"/logos/" + "Eat-Raja_sustainability.jpg"}
+								className="max-w-32 max-h-32"
+							></img>
+						</a>
 					</div>
+				</div>
+				<br />
+				<br />
+				<SponsorText
+					title={<>Radio Partner</>}
+					textStyles="text-center cinzel-font text-amber-200"
+				/>
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
+					<div
+						key={"spon"}
+					//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
+					>
+						<br />
+						<a href={""} target="_blank" rel="noreferrer">
+							<img
+								src={"/logos/" + "indigo-music_Hospitality.jpg"}
+								className="max-w-32 max-h-32"
+							></img>
+						</a>
+					</div>
+				</div>
+				<br />
+				<br />
+				<SponsorText
+					title={<>Social Media Partner</>}
+					textStyles="text-center cinzel-font text-amber-200"
+				/>
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
+					<div
+						key={"spon"}
+					//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
+					>
+						<br />
+						<a href={""} target="_blank" rel="noreferrer">
+							<img
+								src={"/logos/" + "Namma-Karnataka_social.jpg"}
+								className="max-w-32 max-h-32"
+							></img>
+						</a>
+					</div>
+				</div>
+				<br />
+				<br />
+				<SponsorText
+					title={<>Styling Partner</>}
+					textStyles="text-center cinzel-font text-amber-200"
+				/>
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 justify-items-center">
+					<div
+						key={"spon"}
+					//   className="col-12 col-md-auto text-center flex md:flex-col justify-center"
+					>
+						<br />
+						<a href={""} target="_blank" rel="noreferrer">
+							<img
+								src={"/logos/" + "green_trends.png"}
+								className="max-w-32 max-h-32"
+							></img>
+						</a>
+					</div>
+				</div>
 
 
 			</div>
