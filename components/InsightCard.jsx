4397bcb10345bf1350a62d58e5d411af89b8ea6a
Rahul Anil Nayak <whorahulnayak@gmail.com>
2023-06-01 00:23:50 +0530

    almost working

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -8,19 +8,24 @@ import arrowImg from "../public/arrow.svg";
 
 const InsightCard = ({ name, role, index }) => (
 	<motion.div
-		variants={fadeIn("up", "spring", index * 0.3, 1)}
+		variants={fadeIn("up", "tween", 0.2, 1)}
 		className="flex md:flex-row flex-col gap-4"
 	>
 		<div className="w-full flex flex-1 justify-start items-center sm:flex-col flex-row">
 			<div className="flex-1 mx-[15px] my-[10px] md:ml-[50px] flex flex-col justify-center">
 				{/* add Image */}
-				{/* <Image
+				<div 
+					className="flex flex-row justify-center items-center"
+				>
+					<Image
 					src={"/output/001-rahul.jpg"}
 					alt="arrow"
 					width={120}
 					height={120}
 					className="w-[120px] h-[120px] object-cover"
-				/> */}
+					/>
+				</div>
+				
 				<p className="font-normal lg:text-[20px] text-[18px] text-white text-center">
 					{name}
 				</p>
