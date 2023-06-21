fa420e26ad1298ead836ce44954e0410aace73f3
Rahul Anil Nayak <whorahulnayak@gmail.com>
2023-06-22 01:33:36 +0530

    ggs

--- pages/verifyCertificate.js
+++ pages/verifyCertificate.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import styles from "../styles";
 import { motion } from "framer-motion";
-import { TitleText } from "../components";
+import { TitleText,SubtitleText } from "../components";
 import { fadeIn, staggerContainer } from "../utils/motion";
-import { Navbar } from '../components';
+import jsonData from "../utils/file_information.json";
 
 const VerifyCertificate = () => {
   const [showIframe, setShowIframe] = useState(false);
   const [code, setCode] = useState('');
-
+  const [matchingFile, setMatchingFile] = useState({});
   const handleSubmit = (event) => {
     event.preventDefault();
     const search = jsonData.find((file) => file.filename === `${code}.pdf`);
@@ -16,6 +16,10 @@ const VerifyCertificate = () => {
       console.log(search);
       setMatchingFile(search);
       setShowIframe(true);
+    }
+    else{
+        setShowIframe(false);
+        alert("Invalid Certificate Code");
     }
   };
 
@@ -27,14 +31,22 @@ const VerifyCertificate = () => {
             className={`flex flex-col mb-8 `}
           >
             <TitleText
-              title={<>Verify Certificate</>}
-              textStyles="text-center cinzel-font text-hp-yellow mt-8"
+              title={<>Verify Utsav Certificate</>}
+              textStyles="text-center cinzel-font text-hp-yellow mt-3"
             />
-            <div className=" flex flex-row justify-center items-center gap-4 mt-6">
+            {!showIframe && (
+                // add text here
+                <SubtitleText
+                    title={<>Enter Certificate ID</>}
+                    textStyles="text-center cinzel-font text-hp-purple mt-3"
+                />
+             )} 
+
+            <div className=" flex flex-col md:flex-row justify-center items-center gap-3 sm:justify-around">
               <motion.div
                 className="lg:mx-[32px] font-normal text-[18px] text-center text-white"
               >
-                <input type="text" placeholder="UR23OC033" value={code} onChange={(e) => setCode(e.target.value)} className="input input-bordered input-primary w-1/2 h-1/2 p-4 px-10 rounded-lg" />
+                <input type="text" placeholder="UR23OC000" value={code} onChange={(e) => setCode(e.target.value)} className="input input-bordered input-primary w-1/2 h-1/2 p-4 px-10 rounded-lg" />
               </motion.div>
               <button
                 onClick={handleSubmit}
@@ -45,18 +57,21 @@ const VerifyCertificate = () => {
                           hover:border-transparent 
                           btn
                           h-1/2
+                          mt-2
                           rounded"
+                style={{marginTop: "10px"}}
               >
-                Submit
+                View Certificate
               </button>
             </div>
-
-            <div className="container mx-auto mt-8">
+            {showIframe && (
+            <div className="container mx-auto">
               <iframe
                 className="responsive-iframe"
-                src="https://drive.google.com/file/d/1K6teX8llxA_paLIlR-a1TJhq9Yx1hFrC/preview"
+                src={matchingFile.webviewlink}
               ></iframe>
             </div>
+            )}
           </motion.div>
         </section>
       </div >
